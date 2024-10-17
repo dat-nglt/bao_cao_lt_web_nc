170b9bc2649ce171ba0178cc8680a676b50e80c6
lnmhoa <herozonce1@gmail.com>
2024-10-17 19:52:20 +0700

    header-node

--- backEnd/src/controllers/BookControllers.js
+++ backEnd/src/controllers/BookControllers.js
@@ -2,19 +2,19 @@ import express from "express";
 
 
 const getBookPage = (req, res) => {
-    return res.render("book", {data: {title: 'book', page: 'book'}});
+    return res.render("layout", {data: {title: 'book', page: 'book'}});
 }
 
 const addBook = (req, res) => {
-    return res.render("book", {data: {title: 'book', page: 'book'}});
+    return res.render("layout", {data: {title: 'book', page: 'book'}});
 }
 
 const updateBook = (req, res) => {
-    return res.render("book", {data: {title: 'book', page: 'book'}});
+    return res.render("layout", {data: {title: 'book', page: 'book'}});
 }
 
 const deleteBook = (req, res) => {
-    return res.render("book", {data: {title: 'book', page: 'book'}});
+    return res.render("layout", {data: {title: 'book', page: 'book'}});
 }
 
 export default {getBookPage, addBook, updateBook, deleteBook};
