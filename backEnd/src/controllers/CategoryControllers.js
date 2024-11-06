6283b23a06d4cd7281313bf2f3d95469ca9bcb20
lnmhoa <herozonce1@gmail.com>
2024-11-06 12:13:48 +0700

    news-node

--- backEnd/src/controllers/CategoryControllers.js
+++ backEnd/src/controllers/CategoryControllers.js
@@ -1,20 +1,26 @@
-import express from "express";
+import categoryModel from "../models/CategoryModels";
 
-
-const getCategoryPage = (req, res) => {
-    return res.render("layout", {data: {title: 'category', page: 'category'}});
+const getCategoryPage =async (req, res) => {
+    let listCategory = await categoryModel.getAllCategory();
+    return res.render("layout", {data: {title: 'Thể loại', page: 'category', row: listCategory}});
 }
 
-const addCategory = (req, res) => {
-    return res.render("layout", {data: {title: 'category', page: 'category'}});
+const addCategory = async (req, res) => {
+    await categoryModel.createCategory(req.body);
+    res.redirect("/the-loai");
 }
 
 const updateCategory = (req, res) => {
     return res.render("layout", {data: {title: 'category', page: 'category'}});
 }
 
-const deleteCategory = (req, res) => {
-    return res.render("layout", {data: {title: 'category', page: 'category'}});
-}
+const deleteCategory = async (req, res) => {
+    try {
+        await categoryModel.deleteCategory(req.params.id);
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
 export default {getCategoryPage, addCategory, updateCategory, deleteCategory};
---
+++ backEnd/src/models/NewsModels.js
@@ -0,0 +1,49 @@
+import pool from '../configs/connectDB.js'
+
+const getAllNews = async (search = '', sort = 'desc', start = 0, end = 10) => {
+    const [rows, fields] = await pool.execute(
+        'SELECT * FROM news WHERE title LIKE ? ORDER BY id ' + sort + ' LIMIT ?, ?',
+        [`%${search}%`, start, end]
+    );
+    return rows;
+}
+
+const getTotalNews = async (search = '', sort = 'desc') => {
+    const [rows, fields] = await pool.execute(
+        'SELECT * FROM news WHERE title LIKE ? ORDER BY id ' + sort ,
+        [`%${search}%`]
+    );
+    return rows;
+}
+
+
+
+const createCategory = async (data) => {
+    const { name, description } = data;
+    const [result] = await pool.execute(
+        'INSERT INTO category (name, description) VALUES (?, ?)',
+        [name, description]
+    );
+    return result;
+}
+
+const updateCategory = async (id, data) => {
+    const { name, description } = data;
+    
+    const [result] = await pool.execute(
+        'UPDATE category SET name = ?, description = ? WHERE id = ?',
+        [name, description, id]
+    );
+    
+    return result;
+}
+
+const deleteCategory = async (id) => {
+    const [result] = await pool.execute(
+        'DELETE FROM category WHERE id = ?',
+        [id]
+    );
+    return result;
+}
+
+export default { getAllNews, getTotalNews, updateCategory, deleteCategory} 
