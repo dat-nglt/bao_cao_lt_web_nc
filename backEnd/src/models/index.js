154c1349fe2737fac3570e363ae59100f29fc2b7
dat-nglt <dat.nglt@gmail.com>
2024-11-19 06:44:44 +0700

    Thể loại

--- backEnd/src/models/index.js
+++ backEnd/src/models/index.js
@@ -1,15 +1,24 @@
-import sequelize from '../configs/connectDB.js';
-import newsModel from './NewsModels.js';
-import typeNewsModel from './TypeNewsModels.js';
-import userModel from './UserModels.js';
-import bookModel from './BookModels.js';
-import borrowModel from './BorrowModels.js';
+import sequelize from '../configs/connectDB.js'
+import newsModel from './NewsModels.js'
+import typeNewsModel from './TypeNewsModels.js'
+import userModel from './UserModels.js'
+import bookModel from './BookModels.js'
+import borrowModel from './BorrowModels.js'
+import categoryModel from './CategoryModels.js'
 
-newsModel.belongsTo(typeNewsModel, { foreignKey: 'typeId', as: 'type_news' });
-typeNewsModel.hasMany(newsModel, { foreignKey: 'typeId', as: 'news' });
-borrowModel.belongsTo(userModel, { foreignKey: 'userId', as: 'user' });
-borrowModel.belongsTo(bookModel, { foreignKey: 'bookId', as: 'book' });
-userModel.hasMany(borrowModel, { foreignKey: 'userId', as: 'borrow' });
-bookModel.hasMany(borrowModel, { foreignKey: 'bookId', as: 'borrow' });
+newsModel.belongsTo(typeNewsModel, { foreignKey: 'typeId', as: 'type_news' })
+typeNewsModel.hasMany(newsModel, { foreignKey: 'typeId', as: 'news' })
+borrowModel.belongsTo(userModel, { foreignKey: 'userId', as: 'user' })
+borrowModel.belongsTo(bookModel, { foreignKey: 'bookId', as: 'book' })
+userModel.hasMany(borrowModel, { foreignKey: 'userId', as: 'borrow' })
+bookModel.hasMany(borrowModel, { foreignKey: 'bookId', as: 'borrow' })
 
-export { newsModel, typeNewsModel, userModel, bookModel, borrowModel, sequelize };
+export {
+  newsModel,
+  typeNewsModel,
+  categoryModel,
+  userModel,
+  bookModel,
+  borrowModel,
+  sequelize
+}
--- backEnd/src/routes/webRoute.js
+++ backEnd/src/routes/webRoute.js
@@ -1,40 +1,52 @@
-import express from "express";
-import typeNewsControllers from "../controllers/TypeNewsControllers.js";
-import newsControllers from "../controllers/NewsControllers.js";
-import borrowControllers from "../controllers/BorrowControllers.js";
-const router = express.Router();
+import express from 'express'
+import typeNewsControllers from '../controllers/TypeNewsControllers.js'
+import newsControllers from '../controllers/NewsControllers.js'
+import borrowControllers from '../controllers/BorrowControllers.js'
+import UserController from '../controllers/UserController.js'
+import CategoryControllers from '../controllers/CategoryControllers.js'
+const router = express.Router()
 const initWebRoute = (app) => {
   //tài khoản
+  router.get('/dang-nhap', UserController.getLoginPage)
 
   //thể loại
+  router.get('/the-loai', CategoryControllers.getCategoryPage)
+  router.post('/the-loai', CategoryControllers.addCategory)
+  router.delete('/the-loai/:id', CategoryControllers.deleteCategory)
+  router.put('/the-loai/:id', CategoryControllers.updateCategory)
 
   //sách
 
   //mượn trả
-  router.get("/muon-tra", borrowControllers.getBorrowPage);
-  router.post("/muon-tra", borrowControllers.createBorrow);
-  router.post("/muon-tra/update/:id", borrowControllers.updateBorrow);
-  router.post("/muon-tra/cancel/:id", borrowControllers.cancelBorrow);
-  
+  router.get('/muon-tra', borrowControllers.getBorrowPage)
+  router.post('/muon-tra', borrowControllers.createBorrow)
+  router.post('/muon-tra/update/:id', borrowControllers.updateBorrow)
+  router.post('/muon-tra/cancel/:id', borrowControllers.cancelBorrow)
+
   //phí phạt
 
   //phản hồi
 
   //loại tin tức
-  router.get("/the-loai-tin-tuc", typeNewsControllers.getTypeNewsPage);
-  router.post("/the-loai-tin-tuc", typeNewsControllers.createTypeNews);
-  router.post("/the-loai-tin-tuc/update/:id", typeNewsControllers.updateTypeNews);
-  router.post("/the-loai-tin-tuc/delete/:id", typeNewsControllers.deleteTypeNews);
+  router.get('/the-loai-tin-tuc', typeNewsControllers.getTypeNewsPage)
+  router.post('/the-loai-tin-tuc', typeNewsControllers.createTypeNews)
+  router.post(
+    '/the-loai-tin-tuc/update/:id',
+    typeNewsControllers.updateTypeNews
+  )
+  router.post(
+    '/the-loai-tin-tuc/delete/:id',
+    typeNewsControllers.deleteTypeNews
+  )
 
   //tin tức
-  router.get("/tin-tuc", newsControllers.getNewsPage);
-  router.post("/tin-tuc", newsControllers.createNews);
-  router.post("/tin-tuc/update/:id", newsControllers.updateNews);
-  router.post("/tin-tuc/delete/:id", newsControllers.deleteNews);
+  router.get('/tin-tuc', newsControllers.getNewsPage)
+  router.post('/tin-tuc', newsControllers.createNews)
+  router.post('/tin-tuc/update/:id', newsControllers.updateNews)
+  router.post('/tin-tuc/delete/:id', newsControllers.deleteNews)
 
+  router.get('*', (req, res) => res.send('Không có trang này'))
+  return app.use('/', router)
+}
 
-  router.get("*", (req, res) => res.send("Không có trang này"));
-  return app.use("/", router);
-};
-
-export default initWebRoute;
+export default initWebRoute
